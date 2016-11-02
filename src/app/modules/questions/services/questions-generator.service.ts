db34664f1395a1412b48e40b9cadbed0b2300b4e
dormd <dormoshed@gmail.com>
2016-11-02 23:52:02 +0200

    fix question generator

--- src/app/modules/questions/services/questions-generator.service.ts
+++ src/app/modules/questions/services/questions-generator.service.ts
@@ -20,13 +20,16 @@ export class QuestionsGeneratorService {
     }
 
     public getCapitalByCountryQuestion() {
-        const questionA2 = this._getRandomAlpha2();
-        const optionsA2s = this._getRandomAlpha2(4);
+        const questionA2 = this._getRandomAlpha2()[0];
+        const optionsA2s = this._getRandomAlpha2(3);
+        optionsA2s.push(questionA2);
 
-        const question = 'What is the capital of {{ questionA2 | a2ToCountry }}?';
-        const answer = questionA2;  
+        const questionCountryName = this._countriesData[questionA2].name.common;
+        const answer = this._countriesData[questionA2].capital;  
+        const question = `What is the capital of ${ questionCountryName }?`;
+        
         const options = optionsA2s.map((a2: string): string => {
-            return '{{ questionA2 | a2ToCountry }}';
+            return this._countriesData[a2].capital;
         });
 
         return { question, options, answer };
