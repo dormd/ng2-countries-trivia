5c6156438dfa7f46379e6ac5679abfa66d6e952e
dormd <dormoshed@gmail.com>
2016-11-06 21:38:48 +0200

    add riddle type to quiz & flags only configuration

--- src/app/modules/questions/components/quiz/quiz.component.ts
+++ src/app/modules/questions/components/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, Input, OnInit }  from '@angular/core';
-import { QuestionsGeneratorService }         from '../../services';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { QuestionsGeneratorService } from '../../services';
+import { IRiddle } from '../../models';
 
 @Component({
     selector: 'quiz',
@@ -10,10 +11,9 @@ import { QuestionsGeneratorService }         from '../../services';
 export class QuizComponent implements OnInit {
     private _WAITING_TIME = 1250;
 
-    private _question: string;
-    private _options: string[];
-    private _answerIndex: number;
-    
+    private _riddle: IRiddle;
+    private isOnlyFlagRiddles = false;
+
     private _correctNum = 0;
     private _wrongNum = 0;
 
@@ -24,14 +24,12 @@ export class QuizComponent implements OnInit {
     }
 
     private _onOptionClick(optionIndex: number): void {
-        optionIndex === this._answerIndex ? this._correctNum++ : this._wrongNum++;
+        optionIndex === this._riddle.answerIndex ? this._correctNum++ : this._wrongNum++;
         setTimeout(this._generateRiddle, this._WAITING_TIME);        
     }
 
-    private _generateRiddle = () => {
-        const riddle = this._questionsGeneratorService.generateTextRiddle();
-        this._question = riddle.question;
-        this._options = riddle.options;
-        this._answerIndex = riddle.answerIndex;
+    private _generateRiddle = (): void => {
+        console.log(this.isOnlyFlagRiddles);
+        this._riddle = this._questionsGeneratorService.generateRiddle(this.isOnlyFlagRiddles);
     }
 }
