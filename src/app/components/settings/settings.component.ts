6137f74b2b363ac40a217a260a8172fbc28c94f6
dormd <dormoshed@gmail.com>
2016-11-09 22:21:14 +0200

    add settings component

---
+++ src/app/components/settings/settings.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output, EventEmitter }  from '@angular/core';
+
+@Component({
+    selector: 'settings',
+    templateUrl: './settings.component.html',
+    styleUrls: [ './settings.component.css' ],	
+})
+
+export class SettingsComponent {
+    constructor() { }
+}
